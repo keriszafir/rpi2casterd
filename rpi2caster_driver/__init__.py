3ce9b583a27487f60cc0d57209cfaafc1997ca01
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-06-16 17:45:18 +0200

    do away with the module imports

--- rpi2caster_driver/__init__.py
+++ rpi2caster_driver/__init__.py
@@ -1,12 +1,6 @@
 """rpi2caster-driver - a hardware control daemon for rpi2caster interfaces
 based on a Raspberry Pi microcomputer (and other single-board computers)."""
 
-try:
-    from .main import Controller, Interface, APP as app, setup, main
-    __all__ = ['Controller', 'Interface', 'app', 'setup', 'main']
-except ImportError:
-    print('Some dependencies are not installed')
-
 __version__ = '0.1.dev1'
 __author__ = 'Christophe Slychan'
 __author_email__ = 'krzysztof.slychan@gmail.com'
