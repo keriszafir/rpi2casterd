7968e10635574890873ced10b1463be4a6b2e5ce
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-06-16 16:56:59 +0200

    fix being unable to import from root (for setup) if main module cannot import packages

--- rpi2caster_driver/__init__.py
+++ rpi2caster_driver/__init__.py
@@ -1,11 +1,13 @@
 """rpi2caster-driver - a hardware control daemon for rpi2caster interfaces
 based on a Raspberry Pi microcomputer (and other single-board computers)."""
 
-from .main import Controller, Interface, APP as app, setup, main
+try:
+    from .main import Controller, Interface, APP as app, setup, main
+    __all__ = ['Controller', 'Interface', 'app', 'setup', 'main']
+except ImportError:
+    print('Some dependencies are not installed')
 
 __version__ = '0.1.dev1'
 __author__ = 'Christophe Slychan'
 __author_email__ = 'krzysztof.slychan@gmail.com'
 __github_url__ = 'http://github.com/elegantandrogyne/rpi2caster-driver'
-
-__all__ = ['Controller', 'Interface', 'app', 'setup', 'main']
