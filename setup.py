517479d4541abb231ae8e45f2aa7eff5dccc0c9b
elegantandrogyne <krzysztof.slychan@gmail.com>
2017-07-09 00:22:52 +0200

    setup fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ __version__ = '1.0'
 __author__ = 'Christophe Slychan'
 __author_email__ = 'krzysztof.slychan@gmail.com'
 __github_url__ = 'http://github.com/elegantandrogyne/rpi2casterd'
-__dependencies__ = ['RPi.GPIO >= 0.6.3', 'Flask >= 0.12', 'librpi2caster >= 1.0']
+__dependencies__ = ['RPi.GPIO >= 0.6.3', 'Flask >= 0.12',
+                    'librpi2caster >= 1.0']
 
 with open('README.rst', 'r') as readme_file:
     long_description = readme_file.read()
@@ -20,10 +21,12 @@ setup(name='rpi2casterd', version=__version__,
       data_files=[('/etc/systemd/system', ['data/rpi2casterd.service']),
                   ('/etc', ['data/rpi2casterd.conf'])],
       classifiers=['Development Status :: 5 - Production/Stable',
-                   'License :: OSI Approved :: MIT',
+                   'Topic :: System :: Hardware :: Hardware Drivers',
+                   'License :: OSI Approved :: MIT License',
                    'Natural Language :: English',
                    'Operating System :: POSIX :: Linux',
-                   'Programming Language :: Python :: 3 :: Only'],
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Framework :: Flask'],
       install_requires=__dependencies__, zip_safe=True,
       entry_points={'console_scripts': ['rpi2casterd = rpi2casterd.main:main']}
       )
